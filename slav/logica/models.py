ea72f7f1346ae71e668053c6dd61cc3e5b97fa43
marcojrzx <marcojrzx@hotmail.com>
2014-06-28 21:12:18 -0500

    punto previo a ingresar tablas relacionales

--- slav/logica/models.py
+++ slav/logica/models.py
@@ -40,4 +40,23 @@ class Area(models.Model):
 
 def __unicode__(self):
  return self.nombre_ar
-  	
+
+class Subtema(models.Model):
+ nombre_sub = models.CharField(max_length=150)
+
+def __unicode__(self): 
+  return self.nombre_sub
+
+class Nota(models.Model):
+  titulo = models.CharField(max_length=100)
+  fecha = models.DateField(auto_now=False)
+  paginas = models.IntegerField()
+  tipo = models.CharField(max_length=15)
+  municipio = models.CharField(max_length=50)
+  sintesis = models.TextField(max_length=254)
+  texto = models.TextField(max_length=10000)
+
+def __unicode__(self):
+  	return self.titulo
+
+
